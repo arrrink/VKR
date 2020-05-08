bf4b7c4bda6d253e7b7b18f84df6970a7a17ac07
arrrink <arina.nefa@gmail.com>
2020-05-08 11:47:34 +0300

    Initial Commit

---
+++ VKR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKR
+//
+//  Created by Арина Нефёдова on 08.05.2020.
+//  Copyright © 2020 Арина Нефёдова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
